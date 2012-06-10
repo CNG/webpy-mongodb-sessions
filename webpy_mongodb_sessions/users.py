652a3732749777dfc15af0bebcb479795c6cb8b4
Jon Renaut <jon@manfredmacx.com>
2012-06-10 16:52:58 -0400

    enhance password security

--- webpy_mongodb_sessions/users.py
+++ webpy_mongodb_sessions/users.py
@@ -1,7 +1,7 @@
 ## -*- coding: utf-8 -*-
 import web
 import hashlib
-SALTY_GOODNESS = u'5gR3dvtres$@;1£56u&&$"FDergjngrekjkj4223234erwgnkvgdvdfkm'
+import uuid
 
 #Must be set by the thing importing this module
 collection = None
@@ -15,24 +15,30 @@ def get_user():
         return None
 
 def authenticate(username, password):
-    user = collection.find_one({'username':username, 'password':pswd(password)})
-    return user if user else None
+    user = collection.find_one({'username':username})
+    if user and user.get("password") == pswd(password, user.get("salt")):
+        return user
+    return None
 
 def login(user):
     session._user_id = user['_id']
+    session.username = user['username']
     return user
 
 def logout():
     session.kill()
 
 def register(**kwargs):
+    salt = str(uuid.uuid4())
+    kwargs['salt'] = salt
+    kwargs['password'] = pswd(kwargs['password'], salt)
     user = collection.save(kwargs)
     return user
     
-def pswd(password):
-    seasoned = password + SALTY_GOODNESS
+def pswd(password, salt):
+    seasoned = password + salt
     seasoned = seasoned.encode('utf-8')
-    return hashlib.md5(seasoned).hexdigest()
+    return hashlib.sha256(seasoned).hexdigest()
 
 def login_required(function, login_page='/login/'):
     def inner(*args, **kwargs):
