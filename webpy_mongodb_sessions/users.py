f08bfe7cb9d6e3b8604a577dde567740dd8dfd2a
Jon Renaut <jrenaut@gmail.com>
2016-02-16 20:26:48 -0500

    moving towards slightly more secure salt

--- webpy_mongodb_sessions/users.py
+++ webpy_mongodb_sessions/users.py
@@ -6,17 +6,19 @@ import uuid
 #Must be set by the thing importing this module
 collection = None
 session = None
+#TODO: put a real salt in an env variable
+SALT = "aslkjw32immSDK9kwer9sFDk8asdg"
 
 def get_user():
     try:
-        u = collection.find_one({'_id':session._user_id}) 
+        u = collection.find_one({'_id':session._user_id})
         return u
     except AttributeError:
         return None
 
 def authenticate(username, password):
     user = collection.find_one({'username':username})
-    if user and user.get("password") == pswd(password, user.get("salt")):
+    if user and user.get("password") == pswd(password, user.get("username"), user.get("email")):
         return user
     return None
 
@@ -29,14 +31,11 @@ def logout():
     session.kill()
 
 def register(**kwargs):
-    salt = str(uuid.uuid4())
-    kwargs['salt'] = salt
-    kwargs['password'] = pswd(kwargs['password'], salt)
     user = collection.save(kwargs)
     return user
-    
-def pswd(password, salt):
-    seasoned = password + salt
+
+def pswd(password, username, email):
+    seasoned = get_salt(username, email)
     seasoned = seasoned.encode('utf-8')
     return hashlib.sha256(seasoned).hexdigest()
 
@@ -47,3 +46,6 @@ def login_required(function, login_page='/login/'):
         else:
             return web.seeother(login_page+'?next=%s' % web.ctx.get('path','/'))
     return inner
+
+def get_salt(username, email):
+    return "{0}{1}{2}".format(email, SALT, username,)
